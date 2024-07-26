12e88d51abbdae85521a6ea34d5815cbe74e8d20
ayastaga <113945551+ayastaga@users.noreply.github.com>
2024-07-26 10:58:15 -0700

    Add files via upload

---
+++ inc/header.inc.php
@@ -0,0 +1,30 @@
+<nav role="navigation" class="navbar navbar-default real-nav">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand"><img src='images/vmh-logo.png' class='img-responsive img-logo' style="height: 50px; margin-bottom: 100px;" /></a>
+        </div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+
+            <ul class="nav navbar-nav navbar-right">
+
+                <?php if (!User::loggedIn() && !Patient::isPatientIn()) { ?>
+                    <li><a href='login.php' style="font-size: 20px;"><br>Login</a></li>
+                <?php } else {
+                ?>
+
+                    <li><a href='logout.php'><br>Logout</a></li>
+                <?php
+                } ?>
+            </ul>
+
+        </div>
+    </div>
+</nav>
